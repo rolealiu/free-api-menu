7345432fd192edc38f741ff4ca565e8e4af750fa
rolea liu <rolealiu@gmail.com>
2023-08-06 02:34:20 +0800

    Auto sync

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@
 git add .
 
 # 提交更改到本地仓库
-git commit -m "Auto sync $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "Auto sync"
 
 # 推送更改到GitHub仓库，这里假设您的远程仓库名为origin，分支名为master，您可以根据实际情况修改仓库名和分支名
 git push origin master -u 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
